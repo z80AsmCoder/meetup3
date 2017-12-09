3b28ae7375befe8b47b2ee2a8c5e164251b158f5
z80AsmCoder <khemraj.awotar@umail.uom.ac.mu>
2017-12-09 16:18:27 +0400

    Initial import

---
+++ Thread/src/MyThread.java
@@ -0,0 +1,19 @@
+
+public class MyThread extends Thread{
+	
+	private long factorial;
+	
+	public void run() {
+		for(int i = 1; i <=15; i++) {
+			
+		}
+	}
+	
+	public long factorial(int x) {
+		long n = 1;
+		for(int i = x; i>0; i--) {
+			n *= x;
+		}
+	}
+		
+}
